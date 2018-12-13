55758da66a78339312cf7597a14ea797ef04a07e
Joshua Shen <web.maple.sy@gmail.com>
2018-12-13 10:49:04 -0800

    add extension function to uiimageview in HushPhotoPicker

--- TLPhotoPicker/Classes/TLPhotoLibrary.swift
+++ TLPhotoPicker/Classes/TLPhotoLibrary.swift
@@ -10,35 +10,6 @@
 import Foundation
 import Photos
 
-extension UIImageView {
-    
-    func loadImage(_ asset: PHAsset) {
-        guard frame.size != CGSize.zero else {
-            image = TLBundle.podBundleImage(named: "insertPhotoMaterial")
-            return
-        }
-        
-        if tag == 0 {
-            image = TLBundle.podBundleImage(named: "insertPhotoMaterial")
-        } else {
-            PHImageManager.default().cancelImageRequest(PHImageRequestID(tag))
-        }
-        
-        let options = PHImageRequestOptions()
-        options.isNetworkAccessAllowed = true
-        
-        let id = PHImageManager.default().requestImage(
-            for: asset,
-            targetSize: frame.size,
-            contentMode: .aspectFill,
-            options: options) { [weak self] image, _ in
-                self?.image = image
-        }
-        
-        tag = Int(id)
-    }
-}
-
 protocol TLPhotoLibraryDelegate: class {
     func loadCameraRollCollection(collection: TLAssetsCollection)
     func loadCompleteAllCollection(collections: [TLAssetsCollection])
---
+++ UIImageView+Extensions.swift
@@ -0,0 +1,39 @@
+//
+//  UIImageView+Extensions.swift
+//  HushPhotoPicker
+//
+//  Created by Joshua Shen on 12/12/18.
+//
+
+import Foundation
+import Photos
+
+extension UIImageView {
+    
+    func loadImage(_ asset: PHAsset) {
+        guard frame.size != CGSize.zero else {
+            image = TLBundle.podBundleImage(named: "insertPhotoMaterial")
+            return
+        }
+        
+        if tag == 0 {
+            image = TLBundle.podBundleImage(named: "insertPhotoMaterial")
+        } else {
+            PHImageManager.default().cancelImageRequest(PHImageRequestID(tag))
+        }
+        
+        let options = PHImageRequestOptions()
+        options.isNetworkAccessAllowed = true
+        
+        let id = PHImageManager.default().requestImage(
+            for: asset,
+            targetSize: frame.size,
+            contentMode: .aspectFill,
+            options: options) { [weak self] image, _ in
+                self?.image = image
+        }
+        
+        tag = Int(id)
+    }
+    
+}
